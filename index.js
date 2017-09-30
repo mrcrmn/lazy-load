2da6f176e15512d664969921f7f46ba84f9439ad
mrcrmn <marcoreimann@outlook.de>
2017-09-30 14:10:23 +0200

    v.0.1.0 done

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 'use strict';
 
 module.exports = class LazyLoad {
+
 	constructor(options = {
 		addClass: "loaded",
-		data: "img-src"
 	}) {
 		this.options = options;
-	},
+		this.listenForLoad();
+	}
+
 	listenForLoad() {
+		this.loadAndAddClass();
+
 		window.addEventListener("scroll", () => {
-			let els = document.querySelectorAll(`[data-${this.options.data}]:not("${this.options.addClass}")`);
-			els.forEach(el => {
-				consolelog(el);
-			});
+			this.loadAndAddClass();
 		});
 	}
+
+	loadAndAddClass() { 
+		// var t0 = performance.now();
+
+		var els = document.querySelectorAll("[data-img-src]");
+		var loadedClass = this.options.addClass;
+
+		els.forEach(el => {
+
+			if (!el.classList.contains(loadedClass) && el.offsetTop - window.innerHeight <= window.pageYOffset) {
+
+				el.src = el.dataset.imgSrc;
+
+				el.onload = function() {
+					this.classList.add(loadedClass);
+					this.removeAttribute("data-img-src");
+				}
+
+			}
+
+		});
+
+		// console.log(performance.now() - t0);
+
+	}
+
 }
